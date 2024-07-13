c7d6bd1028858232d1317b32a99d5fb79b052d50
Aryantiwarib <143889805+Aryantiwarib@users.noreply.github.com>
2024-07-13 13:26:39 +0530

    Create app.py

---
+++ Parliament Bot/app.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import os
+from langchain_groq import ChatGroq
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from dotenv import load_dotenv
+import time
+
+load_dotenv()
+
+# Load the GROQ and OpenAI API KEY 
+groq_api_key = os.getenv('GROQ_API_KEY')
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+
+st.title("📄🤖 Parliament Bot")
+
+llm = ChatGroq(
+    groq_api_key=groq_api_key,
+    model_name="Llama3-8b-8192"
+)
+
+prompt = ChatPromptTemplate.from_template(
+"""
+Answer the questions based on the provided context only.
+Please provide the most accurate response based on the question
+<context>
+{context}
+<context>
+Questions:{input}
+"""
+)
+
+def vector_embedding():
+    if "vectors" not in st.session_state:
+        st.session_state.embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+        st.session_state.loader = PyPDFDirectoryLoader("./docs")  # Data Ingestion
+        st.session_state.docs = st.session_state.loader.load()  # Document Loading
+        st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)  # Chunk Creation
+        st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state.docs[:20])  # Splitting
+        st.session_state.vectors = FAISS.from_documents(st.session_state.final_documents, st.session_state.embeddings)  # Vector OpenAI embeddings
+
+if st.button("📚 Create Document Embeddings"):
+    vector_embedding()
+    st.success("Vector Store DB is ready!")
+
+# Input text box for the user's question
+prompt1 = st.text_input("💬 Enter Your Question From Documents")
+
+if st.button("📝 Get Answer"):
+    if prompt1:
+        st.write("#### 💬 Your Question:")
+        st.text_area(label="", value=prompt1, height=70, key="question_box")
+
+        document_chain = create_stuff_documents_chain(llm, prompt)
+        retriever = st.session_state.vectors.as_retriever()
+        retrieval_chain = create_retrieval_chain(retriever, document_chain)
+        
+        # start = time.process_time()
+        response = retrieval_chain.invoke({'input': prompt1})
+        # st.write(f"⏱️ Response time: {time.process_time() - start:.2f} seconds")
+        
+        st.write("#### 📄 Answer:")
+        st.text_area(label="", value=response['answer'], height=200, key="answer_box")
+
+        # With a Streamlit expander
+        with st.expander("📚 Document Similarity Search"):
+            # Find the relevant chunks
+            for i, doc in enumerate(response["context"]):
+                st.write(doc.page_content)
+                st.write("--------------------------------")
